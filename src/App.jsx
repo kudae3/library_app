494a071951aea28b4a2b5f422d78e04a9cad4bf4
Kudae Sithu <kudaesithu2@gmail.com>
2024-07-18 16:22:08 +0630

    Added Switch Transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Navigation from "./components/Navigation"
 import './index.css'
+import { SwitchTransition, CSSTransition } from "react-transition-group"
+import './style.css'
 
 function App() {
+  const location = useLocation()
   return (
     <div className="space-y-3">
         <Navigation/>
-        <Outlet/>
+        <SwitchTransition>
+          <CSSTransition timeout={200} classNames="fade" key={location.pathname}>            
+              <Outlet/>            
+          </CSSTransition>
+        </SwitchTransition>
     </div>
   )
 }
